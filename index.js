00ef680c5f8ca140c93b6a35a6e9706218e04779
ledi03 <ledion.lamce@gmail.com>
2026-02-10 14:23:16 +0100

    handling form data, including file uploads

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+import { storage } from './config/multer.js';
 
 const app = express();
+const upload = multer({ storage: storage, limits: { fileSize: 5 * 1024 * 1024 } });
 const port = 3500;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(upload.single('image')); 
 
 app.get('/', (req, res) => {
 
@@ -12,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.post('/form', (req, res) => {
     console.log('Form data received:', req.body);
+    console.log('Uploaded file:', req.file);
     res.send('Form submitted!');
 });
 
